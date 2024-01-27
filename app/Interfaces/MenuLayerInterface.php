22f697a019b7c389cc03e752d55a33ee80bbb1ff
tanim0299 <tanimchy417@gmail.com>
2024-01-27 23:24:10 +0600

    menu_layer

---
+++ app/Interfaces/MenuLayerInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Interfaces\MenuLayerInterface;
+
+use App\Interfaces\BaseInterface;
+
+interface MenuLayerInterface extends BaseInterface {
+
+}
---
+++ app/Repository/MenuLayerRepository.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Repository;
+
+use App\Interfaces\MenuLayerInterface;
+
+class MenuLayerRepository implements MenuLayerInterface {
+
+}
